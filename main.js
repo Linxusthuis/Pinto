332ac311ad9f514be30866702cb9c8ecb0eeb45e
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-10-18 08:58:56 -0300

    aeeds

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ function criaCart(categoria, pergunta, resposta){
   let container = document.getElementById('container');
   let cart = document.createElement('article');
   cart.className = "cart";
-  cart.innerHTML = `
-<div class="cont-cart">
-    <h3>${categoria}</h3>
-    <div class="perg-cart">
-        <p>${pergunta}</p>
+  `
+  cart.innerHTML = 
+    <div class="cont-cart">
+        <h3>${categoria}</h3>
+        <div class="perg-cart">
+            <p>${pergunta}</p>
+        </div>
+        <div class="res-cart">
+            <p>${resposta}</p>
+        </div>
     </div>
-    <div class="res-cart">
-        <p>${resposta}</p>
-    </div>
-</div>`
+    `
     container.appendChild(cart)
 }
