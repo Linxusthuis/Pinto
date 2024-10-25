769a984dcdca21eaa0679c50b25916e9190e3597
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-10-25 12:23:04 -0300

    2
    0

--- main2.js
+++ main2.js
@@ -3,7 +3,7 @@ function criaCart(categoria, pergunta, resposta){
     let cart = document.createElement('article');
     cart.className = "cart";
     cart.innerHTML = `
-  <div class="cont-cart">
+  <div class="cont-cart" on-click="resp(this)">
       <h3>${categoria}</h3>
       <div class="perg-cart">
           <p>${pergunta}</p>
@@ -13,4 +13,13 @@ function criaCart(categoria, pergunta, resposta){
       </div>
   </div>`
   container.appendChild(cart)
+}
+function resp(card){
+    const resposta = card.document.querySelector('.res-cart');
+    if (resposta.style.display === 'none'){
+        resposta.style.display = 'block'
+    }
+    else{
+        resposta.style.display = 'none'
+    }
 }
